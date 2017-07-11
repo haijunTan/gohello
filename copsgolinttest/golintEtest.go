633414706ee773fbb71425c5a1947255fefa2ff6
root <tanhaijun@huawei.com>
2017-07-11 09:28:54 +0800

    modify:add error golang syntax

--- copsgolinttest/golintEtest.go
+++ copsgolinttest/golintEtest.go
@@ -3,6 +3,8 @@ import "os"// 用于获得命令行参数os.Args
 import "fmt"
 
 func main() {
-  cool :="so cool"
+  var x int = 1
+  x += 1
+  var str string = "test"
   fmt.Println("Hello golint test,unuser var..\n")
 }
